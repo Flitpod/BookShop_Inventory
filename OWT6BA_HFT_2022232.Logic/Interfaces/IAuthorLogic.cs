37e7448fd00e89e32a4ca81496a904127e5a0cbd
Flitpod <flitpod@gmail.com>
2023-05-19 23:14:59 +0200

    Logic - Add IAuthorLogic interface

---
+++ OWT6BA_HFT_2022232.Logic/Interfaces/IAuthorLogic.cs
@@ -0,0 +1,19 @@
+﻿using OWT6BA_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Logic.Interfaces
+{
+    public interface IAuthorLogic
+    {
+        void Create(Author item);
+        Author Read(int id);
+        IEnumerable<Author> ReadAll();
+        void Update(Author item);
+        void Delete(int id);
+    }
+}
