ab8b05445e1f36de5396e7c2e8804eb72fac0e9d
Flitpod <flitpod@gmail.com>
2023-05-19 23:08:38 +0200

    Repository / Client - Manual Test repositories ReadAll() - passed

--- OWT6BA_HFT_2022232.Client/Program.cs
+++ OWT6BA_HFT_2022232.Client/Program.cs
@@ -1,4 +1,8 @@
-﻿using OWT6BA_HFT_2022232.Repository.Database;
+﻿using Microsoft.EntityFrameworkCore;
+using OWT6BA_HFT_2022232.Models;
+using OWT6BA_HFT_2022232.Repository.Database;
+using OWT6BA_HFT_2022232.Repository.Interface;
+using OWT6BA_HFT_2022232.Repository.ModelRepositories;
 using System;
 using System.Linq;
 
@@ -10,6 +14,8 @@ namespace OWT6BA_HFT_2022232.Client
         {
             // manual tests during developement
             // BookDbContextTest(); // PASSED
+            // RepositoryTests(); // PASSED
+
         }
 
         /// <summary>
@@ -21,6 +27,24 @@ namespace OWT6BA_HFT_2022232.Client
             var authors = db.Authors.ToArray();
             var categories = db.Categories.ToArray();
             var books = db.Books.ToArray();
+            ;
+        }
+
+        /// <summary>
+        /// Tests - Repository classes work as should
+        /// </summary>
+        static void RepositoryTests()
+        {
+            BookDbContext dbContext = new BookDbContext();
+
+            IRepository<Author> AuthorRepo = new AuthorRepository(dbContext);
+            IRepository<Book> BookRepo = new BookRepository(dbContext);
+            IRepository<Category> CategoryRepo = new CategoryRepository(dbContext);
+
+            var authors = AuthorRepo.ReadAll().ToArray();
+            var books = BookRepo.ReadAll().ToArray();
+            var categories = CategoryRepo.ReadAll().ToArray();
+
             ;
         }
     }
