16a438a9e4ad3ed28f7b499d9a8ea32b2de4eeff
Flitpod <flitpod@gmail.com>
2023-05-19 22:09:13 +0200

    Repository - Add CRUD IRepository interface

---
+++ OWT6BA_HFT_2022232.Repository/Interface/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Repository.Interface
+{
+    public interface IRepository<T>  where T : class
+    {
+        public void Create(T item);
+        public T Read(int id);
+        public IQueryable<T> ReadAll();
+        public void Update(T item);
+        public void Delete(int id);
+    }
+}
