92d4cebe4a8c9abe15f17386f76b1c1ec543f266
Flitpod <flitpod@gmail.com>
2023-05-19 23:16:51 +0200

    Logic - Add ICategoryLogic interface

---
+++ OWT6BA_HFT_2022232.Logic/Interfaces/ICategoryLogic.cs
@@ -0,0 +1,18 @@
+﻿using OWT6BA_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Logic.Interfaces
+{
+    public interface ICategoryLogic
+    {
+        void Create(Category item);
+        Category Read(int id);
+        IEnumerable<Category> ReadAll();
+        void Update(Category item);
+        void Delete(int id);
+    }
+}
