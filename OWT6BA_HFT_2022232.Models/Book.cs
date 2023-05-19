4370977746c1e6ffcc024278591c326658c5c994
Flitpod <flitpod@gmail.com>
2023-05-19 18:48:32 +0200

    Models - Add Book model class

---
+++ OWT6BA_HFT_2022232.Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
+
+        [StringLength(240)]
+        public string Title { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public int Pages { get; set; }
+
+        public double Rating { get; set; }
+        
+        public int NumberOfReviews { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public int CaregoryId { get; set; }
+    }
+}
