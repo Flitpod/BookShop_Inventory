e86bd88a139e96b82f1a3636dfb7096d378397d0
Flitpod <flitpod@gmail.com>
2023-05-19 18:50:44 +0200

    Models - Add Author model class

---
+++ OWT6BA_HFT_2022232.Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Models
+{
+    public class Author
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AuthorId { get; set; }
+
+        [StringLength(50)]
+        string AuthorName { get; set; }
+    }
+}
