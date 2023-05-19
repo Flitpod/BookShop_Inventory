303577345737504f4c903784f4e1b6f8fcd195a7
Flitpod <flitpod@gmail.com>
2023-05-19 22:40:24 +0200

    Repository - Add BookRepository class

---
+++ OWT6BA_HFT_2022232.Repository/ModelRepositories/BookRepository.cs
@@ -0,0 +1,34 @@
+﻿using OWT6BA_HFT_2022232.Models;
+using OWT6BA_HFT_2022232.Repository.Database;
+using OWT6BA_HFT_2022232.Repository.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Repository.ModelRepositories
+{
+    public class BookRepository : Repository<Book>
+    {
+        // ctor
+        public BookRepository(BookDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        // override abstract crud methods
+        public override Book Read(int id)
+        {
+            return dbContext.Books.FirstOrDefault(b => b.BookId == id);
+        }
+
+        public override void Update(Book item)
+        {
+            var old = Read(item.BookId);
+            foreach(var prop in item.GetType().GetProperties()) 
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+        }
+    }
+}
