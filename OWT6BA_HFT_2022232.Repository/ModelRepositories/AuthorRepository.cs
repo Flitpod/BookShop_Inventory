526ee0f5f5d89e58631aacb1cb83e1850b9faf32
Flitpod <flitpod@gmail.com>
2023-05-19 22:36:09 +0200

    Repository - Add AuthorRepository class

---
+++ OWT6BA_HFT_2022232.Repository/ModelRepositories/AuthorRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OWT6BA_HFT_2022232.Models;
+using OWT6BA_HFT_2022232.Repository.Database;
+using OWT6BA_HFT_2022232.Repository.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Repository.ModelRepositories
+{
+    public class AuthorRepository : Repository<Author>
+    {
+        // ctor
+        public AuthorRepository(BookDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        // override abstract crud methods
+        public override Author Read(int id)
+        {
+            return dbContext.Authors.FirstOrDefault(a => a.AuthorId == id);
+        }
+
+        public override void Update(Author item)
+        {
+            var old = Read(item.AuthorId);
+            foreach (var prop in item.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            dbContext.SaveChanges();
+        }
+    }
+}
