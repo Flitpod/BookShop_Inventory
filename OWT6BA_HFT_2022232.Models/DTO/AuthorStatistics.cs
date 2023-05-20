6029fbb2f91dc32ee3ac6ccefdc8ce0c6651af0b
Flitpod <flitpod@gmail.com>
2023-05-20 16:52:28 +0200

    Models - Add DTO classes for the Logic non-CRUD results

---
+++ OWT6BA_HFT_2022232.Models/DTO/AuthorStatistics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Models.DTO
+{
+    public class AuthorStatistics
+    {
+        [StringLength(240)]
+        public string AuthorName { get; set; }
+
+        public double AvgPageNumber { get; set; }
+
+        public double AvgPrice { get; set; }
+
+        public double AvgRating { get; set; }
+    }
+}
---
+++ OWT6BA_HFT_2022232.Models/DTO/CategoryStatistics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT6BA_HFT_2022232.Models.DTO
+{
+    public class CategoryStatistics
+    {
+        [StringLength(240)]
+        public string CategoryName { get; set; }
+
+        public int NumberOfBooks { get; set; }
+
+        public double AvgRating { get; set; }
+
+        public double SumNumberOfReviews { get; set; }
+    }
+}
